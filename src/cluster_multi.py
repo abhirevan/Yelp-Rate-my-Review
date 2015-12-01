ce7b8265682c4ead34bdf499cde0a21dfa32a321
Abhijit Shanbhag <abhirevan@gmail.com>
2015-11-30 19:31:41 -0500

    Checkpoint 11/30

--- src/cluster_multi.py
+++ src/cluster_multi.py
@@ -65,14 +65,16 @@ def print_stats(ip_csv, col):
 
 def extract_sentiment(ip_csv, op_csv):
     # op_csv = ip_csv + "_"
-    with open(ip_csv, "rb") as source, open(op_csv, "wb") as result:
+    with open(ip_csv, "rU") as source, open(op_csv, "wb") as result:
         rdr = csv.reader(source)
         wtr = csv.writer(result)
         wtr.writerow(next(rdr) + ["Polarity"])
         i = 0
         for r in rdr:
+            #print i
             blob = TextBlob(r[1].decode("utf8"))
             wtr.writerow((r) + [blob.sentiment.polarity])
+            #i += 1
 
 
 def kmeans_clustering(ip_csv):
@@ -114,6 +116,31 @@ def agglomerative_clustering(ip_csv, linkage, connectivity):
     os.rename(op_csv, ip_csv)
 
 
+def split_files(op_csv, range):
+    strings = op_csv.split('.csv')
+    op_csv_list = []
+    wrts = []
+    i = 0
+    with open(op_csv, "rb") as source:
+        rdr = csv.reader(source)
+        header = next(rdr)
+        while i < len(range):
+            op_csv_list.append(strings[0] + "_" + str(i) + ".csv")
+            wrts.append(csv.writer(open(op_csv_list[i], "wb")))
+            wrts[i].writerow(header)
+            i += 1
+
+        for r in rdr:
+            l = len(r[1])
+            # find the correct split file
+            i = 0
+            while l > range[i]:
+                i += 1
+            wrts[i].writerow(r)
+
+        return op_csv_list
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Find clustering for csv',
@@ -126,26 +153,28 @@ if __name__ == '__main__':
     ip_csv = args.ip_csv
     op_csv = '{0}_clus.csv'.format(ip_csv.split('.csv')[0])
 
-
     print "-" * 100
-    print "Updating polarity"
+    print "Calculating polarity"
     extract_sentiment(ip_csv, op_csv)
     print "-" * 100
+    print "Splitting files polarity"
+    range = [100, 200, 400, 600, 1000, 1500, 2000, 10000]
+    op_csv_list = split_files(op_csv,range)
 
+    for file in op_csv_list:
+        print "Staring with kmeans++ for ", file
+        kmeans_clustering(file)
+        print "-" * 100
+        print "Results stats for ", file
+        print_stats(file, 4)
+        print "-" * 100
+        command = raw_input("Continue?")
+        if (command == 'n'):
+            break
 
-
-    print "Staring with kmeans++"
-    kmeans_clustering(op_csv)
-    print "-" * 100
-    print "Predicting data"
-    print_stats(op_csv, 4)
-    print "-" * 100
-'''
+    '''
     print "Staring with agglomerative_clustering with ward"
     agglomerative_clustering(op_csv, 'ward', None)
     print_stats(op_csv, 5)
     print "-" * 100
-    print "Staring with agglomerative_clustering with ward"
-    agglomerative_clustering(op_csv, 'ward', None)
-    print_stats(op_csv, 5)
     '''
